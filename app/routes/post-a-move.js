8f8fedc719f2e84132309f970951795a216a4c88
Tim Durward <timothy.a.durward@gmail.com>
2016-04-12 16:19:55 -0700

    transition to sign-in if unauthenticated

--- app/routes/post-a-move.js
+++ app/routes/post-a-move.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   beforeModel: function(){
     console.log(this.get('session'));
     if(!this.get('session.isAuthenticated')){
-      this.transitionTo('application');
+      this.transitionTo('sign-in');
     }
   }
 });
