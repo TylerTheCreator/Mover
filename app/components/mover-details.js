b2886f4a8b62afc8302f6d93b87d23a1c816bb86
Matthew Newell <mnewell10@gmail.com>
2016-04-12 14:59:33 -0700

    not finish and conclude with errors in move details

---
+++ app/components/mover-details.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        type: this.get('type'),
+        itemsList: this.get('itemsList'),
+        numberOfItems: this.get('numberOfItems'),
+        description: this.get('description'),
+        assembly: this.get('assembly'),
+        stairs: this.get('stairs'),
+        image: this.get('image'),
+        toAddress: this.get('toAddress'),
+        fromAddress: this.get('fromAddress'),
+        date: this.get('date'),
+      };
+      this.sendAction('save', params);
+    }
+  }
+});
--- app/routes/post-a-move.js
+++ app/routes/post-a-move.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('move');
+  },
+
+  actions: {
+    save(params) {
+      var newMove = this.store.createRecord('move', params);
+      newMove.save();
+      this.transitionTo('post-a-move');
+    },
+  }
 });
