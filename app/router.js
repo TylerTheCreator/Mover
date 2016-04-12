86cd9071a54fc265d5e92ff3e5941fc4f2f79c74
tylerthecreator <tylerfinleyy@gmail.com>
2016-04-12 09:21:25 -0700

    name fix

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('post-a-home');
+  this.route('post-a-move');
   this.route('sign-up');
   this.route('sign-in');
   this.route('open-moves');
