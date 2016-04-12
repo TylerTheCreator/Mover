5b311cbb298ae4c14f2b21bb171c40c3906ed392
brooke hurford and taylor pokoj <brookebhurford@gmail.com>
2016-04-12 11:51:29 -0700

    Remove sign up route and create mover-sign-up route

--- tests/unit/routes/mover-sign-up-test.js
+++ tests/unit/routes/mover-sign-up-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:sign-up', 'Unit | Route | sign up', {
+moduleFor('route:mover-sign-up', 'Unit | Route | mover sign up', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
