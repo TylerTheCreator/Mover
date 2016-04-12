dd4f622567a5b24a56a8861ed29dfbb38da8301a
Tim Durward <timothy.a.durward@gmail.com>
2016-04-12 15:46:15 -0700

    add application auth route

---
+++ app/routes/application.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+       login: function() {
+         var controller = this.get('controller');
+         var email = controller.get('userEmail');
+         var password = controller.get('userPassword');
+           this.get('session').authenticate('authenticator:firebase', {
+               'email': email,
+               'password': password
+           }).then(function() {
+               this.transitionTo('protected');
+           }.bind(this));
+       },
+       logout: function() {
+           this.get('session').invalidate().then(function() {
+               this.transitionTo('application');
+           }.bind(this));
+       }
+   }
+});
