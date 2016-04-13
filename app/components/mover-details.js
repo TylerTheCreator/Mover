9efb2c0717b69f8f0a461549a03ebc4b3dff2746
Brooke Hurford <brookebhurford@gmail.com>
2016-04-13 13:52:56 -0700

    Merge issues

--- app/components/mover-details.js
+++ app/components/mover-details.js
@@ -4,19 +4,6 @@ export default Ember.Component.extend({
   actions: {
     save() {
       var params = {
-<<<<<<< HEAD
-        type: this.get('type') ? this.get('type') : " ",
-        itemsList: this.get('itemsList') ? this.get('itemsList') : " ",
-        numberOfItems: this.get('numberOfItems') ? this.get('numberOfItems') : " ",
-        description: this.get('description') ? this.get('description') : " ",
-        fragile: this.get('fragile') ? this.get('fragile') : " ",
-        assembly: this.get('assembly') ? this.get('assembly') : " ",
-        stairs: this.get('stairs') ? this.get('stairs') : " ",
-        image: this.get('image') ? this.get('image') : " ",
-        toAddress: this.get('toAddress') ? this.get('toAddress') : " ",
-        fromAddress: this.get('fromAddress') ? this.get('fromAddress') : " ",
-        date: this.get('date') ? this.get('date') : " ",
-=======
         type: this.get('type') ? this.get('type'):"",
         itemsList: this.get('itemsList') ? this.get('type'):"",
         numberOfItems: this.get('numberOfItems') ? this.get('type'):"",
@@ -31,7 +18,6 @@ export default Ember.Component.extend({
         user: this.get('user'),
         latitude: this.get('latitude'),
         longitude: this.get('longitude'),
->>>>>>> master
       };
       this.sendAction('save2', params);
     }
