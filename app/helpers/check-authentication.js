38abf392482f7a96b8b7f58fc18b4b6f6e54c99b
Tim Durward <timothy.a.durward@gmail.com>
2016-04-13 09:42:53 -0700

    push for taylor

--- app/helpers/check-authentication.js
+++ app/helpers/check-authentication.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
+//  import Firebase from 'firebase';
+export function isNotAuthenticated() {
+  var session = this.get('session.isAuthenticated');
+//   var auth = session.isAuthenticated;
 
-export function checkAuthentication(params/*, hash*/) {
-  return params;
+  if (session == false){
+    return 'sign in';
+  }
+  else {
+      return 'didn"t work';
+  } 
+  
 }
 
-export default Ember.Helper.helper(checkAuthentication);
+export default Ember.Helper.helper(isNotAuthenticated);
