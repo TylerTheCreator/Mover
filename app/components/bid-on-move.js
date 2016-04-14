d164876e8f219d334ec883b37e1eb0745fa6c8e3
Taylor Pokoj <pokojt@gmail.com>
2016-04-14 10:57:35 -0700

    Update post a bid form

--- app/components/bid-on-move.js
+++ app/components/bid-on-move.js
@@ -14,6 +14,9 @@ export default Ember.Component.extend({
       };
       if (this.get('name') !== undefined && this.get('bid') !== undefined && this.get('email') !== undefined) {
         this.toggleProperty('biddingOnMove');
+        this.set('name', "");
+        this.set('bid', "");
+        this.set('email', "");
         this.sendAction('saveBid', params);
       }
       else {
