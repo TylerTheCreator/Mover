a8a7a60511ed6ab7f285d5462c74f620540ab5ff
Brooke Hurford <brookebhurford@gmail.com>
2016-04-14 09:31:45 -0700

    Add computed property to open move list to show the amount of bids

--- app/components/move-tile.js
+++ app/components/move-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  totalBids: Ember.computed('move.bids', function(){
+    return this.get('move.bids').get('length');
+  })
 });
