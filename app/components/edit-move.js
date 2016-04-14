9ea949a6742e350622179bd5e516e0d3cdd63a87
Taylor Pokoj <pokojt@gmail.com>
2016-04-14 16:43:42 -0700

    Add functionality to edit a move on user profile page

--- app/components/edit-move.js
+++ app/components/edit-move.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    toggleDetail() {
+      this.toggleProperty('editFormShowing');
+    },
+    editMove(move) {
+      var formInputs = {
+        type: this.get('type'),
+        itemsList: this.get('itemsList'),
+        numberOfItems: this.get('numberOfItems'),
+        description: this.get('description'),
+        fragile: this.get('fragile'),
+        assembly: this.get('assembly'),
+        stairs: this.get('stairs'),
+        image: this.get('image'),
+        toAddress: this.get('toAddress'),
+        fromAddress: this.get('fromAddress'),
+        date: this.get('date'),
+        user: this.get('user'),
+        latitude: this.get('latitude'),
+        longitude: this.get('longitude'),
+      };
+      this.toggleProperty('editFormShowing');
+      this.sendAction('editMove', move, formInputs);
+    }
+  }
 });
--- app/routes/user-profile.js
+++ app/routes/user-profile.js
@@ -9,6 +9,16 @@ export default Ember.Route.extend({
       console.log(move);
       move.destroyRecord();
       this.transitionTo('user-profile');
+    },
+    editMove(move, formInputs) {
+      alert("hey");
+      Object.keys(formInputs).forEach(function(key) {
+        if(formInputs[key]!==undefined) {
+          move.set(key, formInputs[key]);
+        }
+      });
+      move.save();
+      this.transitionTo('user-profile');
     }
   }
 });
